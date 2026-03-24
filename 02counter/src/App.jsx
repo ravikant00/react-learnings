6bc32828cfc4238e3a3ea00db198c38d825167bb
ravikant <chaurasiar307@gmail.com>
2026-03-25 00:19:58 +0530

    An react counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15);
+  // let counter = 15;
+
+  const addValue = () =>{
+    // console.log("clicked", counter);
+    if (counter<20) {
+      // counter = counter+1;
+      // setCounter(counter) 
+      // or
+      setCounter(counter+1)
+    }
+    
+  }
+  const removeValue = () =>{
+    if (counter>0) {
+      // counter = counter-1;
+      setCounter(counter-1)
+    }
+    
+  }
+
+  return (
+    <>
+
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={addValue}>Add Value{counter}</button>
+    <br/>
+    <button onClick={removeValue}>Remove Value{counter}</button>
+
+    <p>Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
